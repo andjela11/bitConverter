80f968961d82182d9272c02d1433b01b78abb4a6
andjela11 <andjela1198@gmail.com>
2022-10-04 13:46:10 +0200

    3rd commit

--- bitConverter/Form1.Designer.cs
+++ bitConverter/Form1.Designer.cs
@@ -31,17 +31,31 @@ namespace bitConverter
         {
             this.numConvert = new System.Windows.Forms.NumericUpDown();
             this.txtConvert = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtCalcFirst = new System.Windows.Forms.TextBox();
+            this.txtCalcSecond = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.radioBtnOkt = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioBtnHex = new System.Windows.Forms.RadioButton();
             this.radioBtnBin = new System.Windows.Forms.RadioButton();
+            this.radioBtnHex = new System.Windows.Forms.RadioButton();
             this.btnConvert = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.numCalc1 = new System.Windows.Forms.NumericUpDown();
+            this.numCalc2 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioBtnXOR = new System.Windows.Forms.RadioButton();
+            this.radioBtnOR = new System.Windows.Forms.RadioButton();
+            this.radioBtnAND = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numConvert)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numCalc1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCalc2)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // numConvert
@@ -58,26 +72,26 @@ namespace bitConverter
             this.txtConvert.Size = new System.Drawing.Size(120, 22);
             this.txtConvert.TabIndex = 1;
             // 
-            // textBox2
+            // txtCalcFirst
             // 
-            this.textBox2.Location = new System.Drawing.Point(617, 278);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(120, 22);
-            this.textBox2.TabIndex = 2;
+            this.txtCalcFirst.Location = new System.Drawing.Point(356, 314);
+            this.txtCalcFirst.Name = "txtCalcFirst";
+            this.txtCalcFirst.Size = new System.Drawing.Size(120, 22);
+            this.txtCalcFirst.TabIndex = 2;
             // 
-            // textBox3
+            // txtCalcSecond
             // 
-            this.textBox3.Location = new System.Drawing.Point(617, 323);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(120, 22);
-            this.textBox3.TabIndex = 3;
+            this.txtCalcSecond.Location = new System.Drawing.Point(356, 359);
+            this.txtCalcSecond.Name = "txtCalcSecond";
+            this.txtCalcSecond.Size = new System.Drawing.Size(120, 22);
+            this.txtCalcSecond.TabIndex = 3;
             // 
-            // textBox4
+            // txtResult
             // 
-            this.textBox4.Location = new System.Drawing.Point(617, 367);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(120, 22);
-            this.textBox4.TabIndex = 4;
+            this.txtResult.Location = new System.Drawing.Point(356, 403);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(120, 22);
+            this.txtResult.TabIndex = 4;
             // 
             // radioBtnOkt
             // 
@@ -102,17 +116,6 @@ namespace bitConverter
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Convert to:";
             // 
-            // radioBtnHex
-            // 
-            this.radioBtnHex.AutoSize = true;
-            this.radioBtnHex.Location = new System.Drawing.Point(6, 57);
-            this.radioBtnHex.Name = "radioBtnHex";
-            this.radioBtnHex.Size = new System.Drawing.Size(57, 21);
-            this.radioBtnHex.TabIndex = 6;
-            this.radioBtnHex.TabStop = true;
-            this.radioBtnHex.Text = "HEX";
-            this.radioBtnHex.UseVisualStyleBackColor = true;
-            // 
             // radioBtnBin
             // 
             this.radioBtnBin.AutoSize = true;
@@ -123,6 +126,17 @@ namespace bitConverter
             this.radioBtnBin.TabStop = true;
             this.radioBtnBin.Text = "BIN";
             this.radioBtnBin.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnHex
+            // 
+            this.radioBtnHex.AutoSize = true;
+            this.radioBtnHex.Location = new System.Drawing.Point(6, 57);
+            this.radioBtnHex.Name = "radioBtnHex";
+            this.radioBtnHex.Size = new System.Drawing.Size(57, 21);
+            this.radioBtnHex.TabIndex = 6;
+            this.radioBtnHex.TabStop = true;
+            this.radioBtnHex.Text = "HEX";
+            this.radioBtnHex.UseVisualStyleBackColor = true;
             // 
             // btnConvert
             // 
@@ -143,17 +157,127 @@ namespace bitConverter
             this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 8;
             // 
+            // numCalc1
+            // 
+            this.numCalc1.Location = new System.Drawing.Point(341, 87);
+            this.numCalc1.Name = "numCalc1";
+            this.numCalc1.Size = new System.Drawing.Size(79, 22);
+            this.numCalc1.TabIndex = 9;
+            // 
+            // numCalc2
+            // 
+            this.numCalc2.Location = new System.Drawing.Point(433, 87);
+            this.numCalc2.Name = "numCalc2";
+            this.numCalc2.Size = new System.Drawing.Size(79, 22);
+            this.numCalc2.TabIndex = 10;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioBtnXOR);
+            this.groupBox2.Controls.Add(this.radioBtnOR);
+            this.groupBox2.Controls.Add(this.radioBtnAND);
+            this.groupBox2.Location = new System.Drawing.Point(356, 128);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(156, 116);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bitwise operation:";
+            // 
+            // radioBtnXOR
+            // 
+            this.radioBtnXOR.AutoSize = true;
+            this.radioBtnXOR.Location = new System.Drawing.Point(6, 84);
+            this.radioBtnXOR.Name = "radioBtnXOR";
+            this.radioBtnXOR.Size = new System.Drawing.Size(59, 21);
+            this.radioBtnXOR.TabIndex = 7;
+            this.radioBtnXOR.TabStop = true;
+            this.radioBtnXOR.Text = "XOR";
+            this.radioBtnXOR.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnOR
+            // 
+            this.radioBtnOR.AutoSize = true;
+            this.radioBtnOR.Location = new System.Drawing.Point(6, 57);
+            this.radioBtnOR.Name = "radioBtnOR";
+            this.radioBtnOR.Size = new System.Drawing.Size(50, 21);
+            this.radioBtnOR.TabIndex = 6;
+            this.radioBtnOR.TabStop = true;
+            this.radioBtnOR.Text = "OR";
+            this.radioBtnOR.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnAND
+            // 
+            this.radioBtnAND.AutoSize = true;
+            this.radioBtnAND.Location = new System.Drawing.Point(6, 30);
+            this.radioBtnAND.Name = "radioBtnAND";
+            this.radioBtnAND.Size = new System.Drawing.Size(58, 21);
+            this.radioBtnAND.TabIndex = 5;
+            this.radioBtnAND.TabStop = true;
+            this.radioBtnAND.Text = "AND";
+            this.radioBtnAND.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(353, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(313, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(313, 359);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 14;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(362, 278);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 24);
+            this.btnCalc.TabIndex = 15;
+            this.btnCalc.Text = "Calc";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(313, 403);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 16;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(616, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.numCalc2);
+            this.Controls.Add(this.numCalc1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtCalcSecond);
+            this.Controls.Add(this.txtCalcFirst);
             this.Controls.Add(this.txtConvert);
             this.Controls.Add(this.numConvert);
             this.Name = "Form1";
@@ -161,6 +285,10 @@ namespace bitConverter
             ((System.ComponentModel.ISupportInitialize)(this.numConvert)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numCalc1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCalc2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,15 +298,26 @@ namespace bitConverter
 
         private System.Windows.Forms.NumericUpDown numConvert;
         private System.Windows.Forms.TextBox txtConvert;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtCalcFirst;
+        private System.Windows.Forms.TextBox txtCalcSecond;
+        private System.Windows.Forms.TextBox txtResult;
         private System.Windows.Forms.RadioButton radioBtnOkt;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radioBtnBin;
         private System.Windows.Forms.RadioButton radioBtnHex;
         private System.Windows.Forms.Button btnConvert;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numCalc1;
+        private System.Windows.Forms.NumericUpDown numCalc2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioBtnXOR;
+        private System.Windows.Forms.RadioButton radioBtnOR;
+        private System.Windows.Forms.RadioButton radioBtnAND;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- bitConverter/Form1.cs
+++ bitConverter/Form1.cs
@@ -19,24 +19,43 @@ namespace bitConverter
 
         private void btnConvert_Click(object sender, EventArgs e)
         {
+            label1.Text = "";
+
             decimal br = numConvert.Value;
-            decimal res = 0M; string r;
+            decimal res = 0M; 
 
             if (radioBtnOkt.Checked)
-                res = ConvertOct(br);
+                txtConvert.Text = ConvertOct(br).ToString();
             else if (radioBtnHex.Checked)
-                r = ConvertHex(br);
+            {
+                txtConvert.Text = ConvertHex(br);
+            }
             else if (radioBtnBin.Checked)
-                res = ConvertBin(br);
+                txtConvert.Text = ConvertBin(br).ToString();
             else
                 label1.Text = "Izaberi osnovu";
-
-            txtConvert.Text = res.ToString();
+            
+           
         }
 
-        private decimal ConvertBin(decimal br)
+        private decimal ConvertBin(decimal num)
         {
-            throw new NotImplementedException();
+            decimal res = 0, ost, r = 1;
+            int osn = 2;
+
+            int br = Decimal.ToInt32(num);
+
+            do
+            {
+                ost = br % osn; //ost = 1
+                br /= osn; //num = 0
+                res += r * ost;
+                r *= 10;
+
+            }
+            while (br != 0);
+
+            return res;
         }
 
         private decimal ConvertOct(decimal num) //10 i 8
@@ -62,54 +81,61 @@ namespace bitConverter
             return res;
         }
 
-        private string ConvertHex(decimal num) //10 i 8
+        private string ConvertHex(decimal num) 
         {
-            string res = "";
-            decimal ost, r = 1, osn = 16, re=0M;
+            int br = Decimal.ToInt32(num);
+            
+            string hex = br.ToString("X");
+            
 
-            if (num >= 0 || num <= 9)
-                return num.ToString();
-            else if (num > 9 || num < 16)
-            {
-                switch (num)
-                {
-                    case 10:
-                        return res = "A";
-                        break;
-                    case 11:
-                        return res = "B";
-                        break;
-                    case 12:
-                        return res = "C";
-                        break;
-                    case 13:
-                        return res = "D";
-                        break;
-                    case 14:
-                        return res = "E";
-                        break;
-                    case 15:
-                        return res = "F";
-                        break;
-                }
-            }
+            return hex;
+
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            label3.Text = numCalc1.Value.ToString();
+            label4.Text = numCalc2.Value.ToString();
+            int br1 = Decimal.ToInt32(numCalc1.Value), br2 = Decimal.ToInt32(numCalc2.Value);
+
+            txtCalcFirst.Text = ConvertBin(numCalc1.Value).ToString();
+            txtCalcSecond.Text = ConvertBin(numCalc2.Value).ToString();
+
+            int r = 0;
+            if (radioBtnAND.Checked)
+                r = Calc(br1, br2, '&');
+            else if (radioBtnOR.Checked)
+                r = Calc(br1, br2, '|');
+            else if (radioBtnXOR.Checked)
+                r = Calc(br1, br2, '^');
             else
+                label2.Text = "Izaberi operaciju";
+
+            label5.Text = r.ToString();
+            txtResult.Text = ConvertBin(r).ToString();
+
+        }
+
+        private int Calc(int br1, int br2, char op)
+        { 
+            switch(op)
             {
+                case '&':
+                    return br1 & br2;
+                    break;
 
-                do
-                {
-                    ost = num % osn;
-                    num /= osn;
-                    re += r * ost;
-                    r *= 10;
+                case '|':
+                    return br1 | br2;
+                    break;
 
-                }
-                while (num != 0);
+                case '^':
+                    return br1 ^ br2;
+                    break;
 
+                default: return -1;
             }
-            return res;
-
-
         }
     }
 }
